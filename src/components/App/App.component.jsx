600ed7d511436a4fef855f689ab5bae6441a9696
Carlos Mori <cmori.94@gmail.com>
2020-04-08 18:30:12 -0300

    feat: basic folder structure
    -Create basic components

---
+++ src/components/App/App.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import PostList from "../PostList/PostList.component";
+
+const App = () => (
+  <div>
+    <h1>Reddit Top Posts</h1>
+    <PostList />
+  </div>
+);
+
+export default App;
---
+++ src/components/Post/Post.component.jsx
@@ -0,0 +1,7 @@
+import React, { useCallback } from "react";
+
+const Post = () => {
+  return <div>Post!</div>;
+};
+Post.propTypes = {};
+export default Post;
